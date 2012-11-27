5ed472d6a7a7dd9dfb69ffedd1201f5653bd73fa
Wallace Mooncai <wmooncai@alumni.rutgers.edu>
2012-11-27 14:01:00 -0500

    Homework Finished.

---
+++ bma-week-11-ObjectiveC/bma-week-11-ObjectiveC.h
@@ -0,0 +1,16 @@
+//
+//  bma-week-11-ObjectiveC.h
+//  bma-week-11-ObjectiveC
+//
+//  Created by Wally on 11/27/12.
+//  Copyright (c) 2012 BigNerdRanch. All rights reserved.
+//
+
+#ifndef bma_week_11_ObjectiveC_bma_week_11_ObjectiveC_h
+#define bma_week_11_ObjectiveC_bma_week_11_ObjectiveC_h
+
+void BigNerdRanch_Ch14();
+void BigNerdRanch_Ch13();
+void BigNerdRanch_Ch12();
+
+#endif
--- bma-week-11-ObjectiveC/main.m
+++ bma-week-11-ObjectiveC/main.m
@@ -2,20 +2,73 @@
 //  main.m
 //  bma-week-11-ObjectiveC
 //
-//  Created by Wally on 11/27/12.
-//  Copyright (c) 2012 BigNerdRanch. All rights reserved.
+//  Created by W. Mooncai on 11/27/12.
+//  Copyright (c) 2012 W. Mooncai. All rights reserved.
 //
 
+/*
+ 
+ Organizer - Date object - see how multiple args work - order? (it mattered in class)
+ 
+ + Challenge Exercises: 12, 13, 14
+ + Read Ch 17 - 25
+ 
+ */
+
 #import <Foundation/Foundation.h>
+#import "bma-week-11-ObjectiveC.h"
+
+void BigNerdRanch_Ch12()
+{
+    NSDateComponents *myBDayComps = [[NSDateComponents alloc] init];
+    [myBDayComps setYear:1971];
+    [myBDayComps setMonth:7];
+    [myBDayComps setDay:12];
+    [myBDayComps setHour:14];
+    [myBDayComps setMinute:14];
+    [myBDayComps setSecond:14];
+    
+    NSCalendar *cal = [[NSCalendar alloc]
+                       initWithCalendarIdentifier:NSGregorianCalendar];
+    NSDate *myDoB = [cal dateFromComponents:myBDayComps];
+    
+    NSDate *now = [NSDate date];
+    
+    double diff = [now timeIntervalSinceDate:myDoB];
+    
+    NSLog(@"\n\nBNR Chapter 12\n\nCurrent Date: %@\n      My DoB: %@\nAge (units):\n  - Seconds: %f\n  - Minutes: %f\n  -   Hours: %f\n  -    Days: %f\n  -   Years: %f\n\n"
+          , now, myDoB, diff
+          , (diff / 60)
+          , (diff / (60 * 60))
+          , (diff / (60 * 60 * 24))
+          , (diff / (60 * 60 * 24 * 365)));
+}
+
+void BigNerdRanch_Ch13()
+{
+    NSTimeZone *tZone = [NSTimeZone systemTimeZone];
+    
+    NSLog(@"\n\nBNR Chapter 13\n\n       Time Zone: %@\nDaylight Savings: %s\n\n"
+          , [tZone name], ([tZone isDaylightSavingTime]) == 0 ? "Yes" : "No");
+}
+
+void BigNerdRanch_Ch14()
+{
+    NSHost *myHost = [NSHost currentHost];
+    
+    NSLog(@"\n\nBNR Chapter 14\n\nThis host name: %@"
+          , [myHost localizedName]);
+}
 
 int main(int argc, const char * argv[])
 {
-
     @autoreleasepool {
         
-        // insert code here...
-        NSLog(@"Hello, World!");
+        BigNerdRanch_Ch12();
         
+        BigNerdRanch_Ch13();
+        
+        BigNerdRanch_Ch14();
     }
     return 0;
 }
